3e1668d6ecb78da303884d9fc4b0c02281c2574e
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-11-07 20:00:55 +0530

    adding ws host - FE

--- FE/src/pages/Chat.jsx
+++ FE/src/pages/Chat.jsx
@@ -37,7 +37,7 @@ const Chat = () => {
   }, []);
 
   const connectWs = () => {
-    const ws = new WebSocket("ws://localhost:3000");
+    const ws = new WebSocket("ws://cchat-server.codewithkara.comhttps://cchat-server.codewithkara.com");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
