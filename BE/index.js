b12697698d6f812d42082a0457dddf7d552ea16e
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-11-07 13:17:50 +0530

    adding custom domain - FE

--- BE/index.js
+++ BE/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://www.chitchatly.codewithkara.com"],
     credentials: true,
   })
 );
