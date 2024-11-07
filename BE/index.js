22bbd34a034a6c20ce247a411af7622a78b8e178
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-11-07 13:34:56 +0530

    adding logo - FE

--- BE/index.js
+++ BE/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://www.chitchatly.codewithkara.com"],
+    origin: ["http://localhost:5173", "https://chitchatly.codewithkara.com"],
     credentials: true,
   })
 );
